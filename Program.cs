5c7631070354e62b235974aaee8ea4c424281fca
hummusbird <birb@birb.cc>
2024-04-30 09:41:30 +0100

    switch to aspnet

--- Program.cs
+++ Program.cs
@@ -1,9 +1,37 @@
-﻿namespace webring;
+using Microsoft.AspNetCore.HttpOverrides;
 
-class Program
+string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy =>
+        {
+            policy.WithOrigins("*").AllowAnyHeader().AllowAnyMethod();
+        });
+});
+
+WebApplication app = builder.Build();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
+
+app.UseHttpsRedirection();  // redirect 80 to 443
+app.UseDefaultFiles();      // use index.html & index.css
+app.UseStaticFiles();       // enable static file serving
+app.UseCors(MyAllowSpecificOrigins);
+
+app.MapGet("/next", async Task<IResult> (HttpRequest request) =>
+{
+    return "next";
+});
+
+app.MapGet("/prev", async Task<IResult> (HttpRequest request) =>
+{
+    return "prev";
+});
+
+app.Run();
